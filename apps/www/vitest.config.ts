7759a6ba306f91b3d9eb9640203011638f819f20
busyhe <busyhe@qq.com>
2025-03-20 12:58:56 +0800

    feat: add typeconfig libs

--- apps/www/vitest.config.ts
+++ apps/www/vitest.config.ts
@@ -1,14 +1,16 @@
 import { fileURLToPath } from 'node:url'
-import { mergeConfig, defineConfig, configDefaults } from 'vitest/config'
+import { defineConfig, configDefaults } from 'vitest/config'
 import viteConfig from './vite.config'
 
-export default mergeConfig(
-  viteConfig,
-  defineConfig({
+export default defineConfig(async (configEnv) => {
+  const resolvedViteConfig = await viteConfig(configEnv)
+
+  return {
+    ...resolvedViteConfig,
     test: {
       environment: 'jsdom',
       exclude: [...configDefaults.exclude, 'e2e/**'],
       root: fileURLToPath(new URL('./', import.meta.url))
     }
-  })
-)
+  }
+})
