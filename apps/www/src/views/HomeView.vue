5fd02f00721f3e5041b05cf5b436a30553666c9a
busyhe <busyhe@qq.com>
2025-04-08 21:02:59 +0800

    feat: add @admin/components

--- apps/www/src/views/HomeView.vue
+++ apps/www/src/views/HomeView.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
+import { Loading } from '@admin/components'
+
 import { ref } from 'vue'
 
 const msg = ref('Hello World')
 </script>
 
 <template>
+  <Loading />
   <div class="mx-auto max-w-md text-3xl font-bold">{{ msg }}</div>
 </template>
---
+++ packages/components/src/loading/Loading.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+interface LoadingProps {
+  class?: string
+  minLoadingTime?: number
+  spinning?: boolean
+  text?: string
+}
+
+defineOptions({ name: 'Loading' })
+
+const props = defineProps<LoadingProps>()
+</script>
+<template>
+  <div>
+    loading
+    <slot></slot>
+  </div>
+</template>
